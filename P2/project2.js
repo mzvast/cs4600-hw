8fa10da0e032d1eeeb5d3d7e0af450de29ce45d3
mzvast <mzvast@gmail.com>
2022-10-16 14:41:20 +0800

    finish p2

--- P2/project2.js
+++ P2/project2.js
@@ -3,7 +3,29 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	const scaleMat = [
+		scale,0,0,
+		0,scale,0,
+		0,0,1
+	]
+
+	const rad = rotation/180*Math.PI;
+	const rotationMat = [
+		Math.cos(rad),Math.sin(rad),0,
+		-Math.sin(rad),Math.cos(rad),0,
+		0,0,1
+	]
+
+	const translateMat = [
+		1,0,0,
+		0,1,0,
+		positionX,positionY,1
+	]
+
+	let t = ApplyTransform(scaleMat,rotationMat);
+	t = ApplyTransform(t,translateMat);
+
+	return t;
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -11,5 +33,19 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	let t1 = trans1,t2 = trans2;
+
+	return [
+		t2[0]*t1[0]+t2[3]*t1[1]+t2[6]*t1[2],// 0
+		t2[1]*t1[0]+t2[4]*t1[1]+t2[7]*t1[2],// 1
+		t2[2]*t1[0]+t2[5]*t1[1]+t2[8]*t1[2],// 2
+
+		t2[0]*t1[3]+t2[3]*t1[4]+t2[6]*t1[5],// 3
+		t2[1]*t1[3]+t2[4]*t1[4]+t2[7]*t1[5],// 4
+		t2[2]*t1[3]+t2[5]*t1[4]+t2[8]*t1[5],// 5
+		
+		t2[0]*t1[6]+t2[3]*t1[7]+t2[6]*t1[8],// 6
+		t2[1]*t1[6]+t2[4]*t1[7]+t2[7]*t1[8],// 7
+		t2[2]*t1[6]+t2[5]*t1[7]+t2[8]*t1[8],// 8
+		]
 }
